0d9a07717f655af56fefe1610d7c6e1430a6ce4b
Mika Reich <mikareich0@gmail.com>
2022-03-07 21:55:02 +0100

    design home hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,19 @@
 import type { NextPage } from 'next'
+import homeStyles from '../styles/home.module.scss';
 
 const Home: NextPage = () => (
-  <div>
-    <header>
-      <h1 className={}>Quizzy</h1>
+  <div className="container">
+    <header className={homeStyles.header}>
+      <h1>Quizzy</h1>
       <span>
         made by <a href="https://github.com/mikareich">@mikareich</a>
       </span>
     </header>
-    <p>
+    <h5 className={homeStyles.description}>
       Play fun little quizzes and test your knowledge about various topics. You
       even can create your <a href="#">own custom quizzes</a> and share them
       with others.
-    </p>
-    <main>
-      <form>
-        <input type="search" placeholder="Search for quiz..." />
-      </form>
-      <ul>
-        <li>
-          <span>🏳️‍🌈 Länderflaggen</span>
-          <span>Wie gut kennst du dich mit Flaggen aus?</span>
-          <span>20 Questions</span>
-          <button>Start Quiz</button>
-        </li>
-        <li>
-          <span>🏳️‍🌈 Länderflaggen</span>
-          <span>Wie gut kennst du dich mit Flaggen aus?</span>
-          <span>20 Questions</span>
-          <button>Start Quiz</button>
-        </li>
-        <li>
-          <span>🏳️‍🌈 Länderflaggen</span>
-          <span>Wie gut kennst du dich mit Flaggen aus?</span>
-          <span>20 Questions</span>
-          <button>Start Quiz</button>
-        </li>
-        <li>
-          <span>🏳️‍🌈 Länderflaggen</span>
-          <span>Wie gut kennst du dich mit Flaggen aus?</span>
-          <span>20 Questions</span>
-          <button>Start Quiz</button>
-        </li>
-        <li>
-          <span>🏳️‍🌈 Länderflaggen</span>
-          <span>Wie gut kennst du dich mit Flaggen aus?</span>
-          <span>20 Questions</span>
-          <button>Start Quiz</button>
-        </li>
-      </ul>
-    </main>
+    </h5>
   </div>
 )
 
