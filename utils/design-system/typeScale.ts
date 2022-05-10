54be0d690a5533950b80533a36b0e9b964df5291
Mika Reich <mikareich0@gmail.com>
2022-05-10 14:39:28 +0200

    add type scale and font family

---
+++ utils/design-system/typeScale.ts
@@ -0,0 +1,27 @@
+// https://type-scale.com/
+
+/** Type scale on desktop devices */
+export const defaultTypeScale = {
+  title: '4rem',
+  h1: '3.052rem',
+  h2: '2.441rem',
+  h3: '1.953rem',
+  h4: '1.563rem',
+  h5: '1.25rem',
+  textLg: '1.125rem',
+  text: '1rem',
+  textSm: '0.875rem',
+}
+
+/** Type scale on mobile devices */
+export const mobileTypeScale = {
+  title: '3.052rem',
+  h1: '2.441rem',
+  h2: '1.953rem',
+  h3: '1.563rem',
+  h4: '1.25rem',
+  h5: '1.125rem',
+  textLg: '1rem',
+  text: '0.875rem',
+  textSm: '0.75rem',
+}
---
+++ utils/design-system/typography.ts
@@ -0,0 +1,4 @@
+/** Font for headings and text */
+const primaryFont = "'Montserrat', sans-serif"
+
+export default primaryFont
